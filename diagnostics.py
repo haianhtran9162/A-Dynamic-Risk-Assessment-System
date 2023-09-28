a6d318bb5ccc6b3aa7b6d4c3c8369725579442cb
anhth <anhth.fuhn@gmail.com>
2023-09-28 22:42:06 +0700

    finish step 4

--- diagnostics.py
+++ diagnostics.py
@@ -1,43 +1,138 @@
 
+import pickle
+import subprocess
 import pandas as pd
 import numpy as np
 import timeit
 import os
 import json
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-##################Load config.json and get environment variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = config['output_folder_path']
+test_data_path = config['test_data_path']
+output_model_path = config['output_model_path']
+output_folder_path = config['output_folder_path']
 
-##################Function to get model predictions
 def model_predictions():
-    #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    """
+        Function read production model, data test and calulate predictions
+        Return: the y predictions (float)
+    """
+    try:
+        logging.info("Reading data test")
+        df_test = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+        X_test = df_test[["lastmonth_activity", "lastyear_activity", "number_of_employees"]]
+        
+        logging.info("Load the production model")
+        model = pickle.load(open(os.path.join(output_model_path, 'trainedmodel.pkl'),'rb'))
+        
+        logging.info("Predict the data test")
+        y_pred = model.predict(X_test)
+        return y_pred
+    except Exception as e:
+        logging.error("Error in model_predictions function: ", e)
+        return None
 
-##################Function to get summary statistics
 def dataframe_summary():
-    #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    """
+        Fuction calculator summary statistics of the dataframe
+        Return: 
+            list value of mean, median, standard deviation (list)
+            dict value of Nan value in dataset (dict)
+    """
+    try:
+        logging.info("Load the ingestion data")
+        df = pd.read_csv(os.path.join(output_folder_path, 'finaldata.csv'))
+        X = df[["lastmonth_activity", "lastyear_activity", "number_of_employees"]]
+        logging.info("Calculating the mean, median and standard deviation scores")
+        mean = X.mean()
+        median = X.median()
+        std = X.std()
+        summary_statistics = []
+        for col in X.columns:
+            summary_statistics.append({
+                col : {
+                    "mean_value": mean[col],
+                    "median_value": median[col],
+                    "std": std[col]
+                }
+            })
+        logging.info("Calculating the missing data value percentage")
+        total_missing = df.isna().sum()
+        precentage_missing = total_missing/df.shape[0]
+        return summary_statistics, precentage_missing.to_dict()
+    except Exception as e:
+        logging.error("Error in dataframe_summary function: ", e)
+        return None
 
-##################Function to get timings
 def execution_time():
-    #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
-
-##################Function to check dependencies
+    """
+        Function calculate timing of training.py and ingestion.py
+        Return:
+            a list of 2 timing values in seconds (list)
+    """
+    try:
+        logging.info("Calculate timing of ingestion.py")
+        start_time = timeit.default_timer()
+        os.system('python3 ingestion.py')
+        ingestion_time = timeit.default_timer() - start_time
+        
+        logging.info("Calculate timing of traning.py")
+        start_time = timeit.default_timer()
+        os.system('python3 training.py')
+        training_time = timeit.default_timer() - start_time
+        
+        return [ingestion_time, training_time]
+    except Exception as e:
+        logging.error("Error in execution_time function: ", e)
+        return None
+    
 def outdated_packages_list():
-    #get a list of 
-
+    """
+        Get the list of outdated packages from the pip installation 
+    """
+    try:
+        logging.info("Get the list of outdated packages from the pip installation")
+        outdate_list = subprocess.run(['pip', 'list', '--outdated'], stdout=subprocess.PIPE, text=True)
+        outdated_packages = []
+        for line in outdate_list.stdout.split('\n')[2:-1]:
+            outdated_packages.append({
+                "package" : line.split()[0],
+                "current_version" : line.split()[1],
+                'latest_version' : line.split()[2]
+            })
+        return outdated_packages
+    except Exception as e:
+        logging.error("Error in outdated_packages_list function: ", e)
+        return None
 
 if __name__ == '__main__':
-    model_predictions()
-    dataframe_summary()
-    execution_time()
-    outdated_packages_list()
-
+    print("Make predictions for an input dataset using the current deployed model:")
+    print(model_predictions())
+    print("________________________________________________________________")
+    
+    summary_statistics, precentage_missing = dataframe_summary()
+    print("Calculate summary statistics (mean, median, and standard deviation) for each numeric column in a dataset:")
+    print(summary_statistics)
+    print("________________________________________________________________")
+    print("Calculate the percent of each column that consists of NA values:")
+    print(precentage_missing)
+    print("________________________________________________________________")
+    
+    count_time = execution_time()
+    print("Measure the timing of important ML tasks (data ingestion and training):")
+    print("Ingestion time: ", count_time[0])
+    print("Training time: ", count_time[1])
+    print("________________________________________________________________")
+    
+    print("Check whether the module dependencies are up-to-date:")
+    print(outdated_packages_list())
+    print("________________________________________________________________")
 
 
 
